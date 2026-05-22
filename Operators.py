cbbd94fc5aefb625fa95ab4fce961c094ec83bf1
sai-teja-climentos <saiteja.b@climentos.com>
2026-05-22 14:25:23 +0530

    Day-2
    Day-2++

---
+++ Operators.py
@@ -0,0 +1,139 @@
+# ========================================================
+# 1. Arithmetic Operators
+# ========================================================
+
+# + (Addition)
+# a = 10
+# b = 20
+
+# c = a + b
+
+# print(c)
+
+
+# - (Subtraction)
+# a = 10
+# b = 20
+
+# c = b - a
+
+# print(c)
+
+# * (Multiplication)
+# a = 10
+# b = 20
+
+# c = a * b
+
+# print(c)
+
+# / (Division)
+# a = 10
+# b = 20
+
+# c = b / a
+
+# print(c)
+
+# % (Modulus)
+# a = 10
+# b = 20
+
+# c = b % a
+
+# print(c)
+
+# ** (Exponent)
+# a = 10
+# b = 14
+
+# c = a ** b
+
+# print(c)
+
+
+# // (Floor Division)
+
+# a = 20
+# b = 30
+
+# c = a // b
+
+# print(c)
+
+
+# ========================================================
+# 2. Comparison (Relational) Operators
+# ========================================================
+# == (Equal to)
+
+# a = 10
+# b = 20
+
+# c = a == b
+# print(c)
+
+# != (Not equal to)
+# a = 10
+# b = 20
+
+# c = a != b
+# print(c)
+
+# > (Greater than) < (Less than)
+# a = 10
+# b = 20
+# c = a < b
+# d = a > b
+
+# print(c)
+# print(d)
+
+# >= (Greater than or equal to) <= (Less than or equal to)
+
+# a = 10
+# b = 10
+
+# c = a >= b
+# d = a <= b
+
+# print(c)
+# print(d)
+
+# ========================================================
+# 3. Logical Operators
+# ========================================================
+# and → True if both conditions are true
+# a = 10
+# b = 12
+
+# c = (b==0) != a  and (a==10000) != a
+
+# print(c)
+# or → True if at least one condition is true
+# a = 10
+# b = 12
+
+# c =  a==11 or a<=10
+
+# print(c)
+
+
+# not → Reverses the result
+
+# a = 12
+# b = 12
+
+# c = not (a <= b)
+
+# print(c)
+
+# =======================================
+# 4. Assignment Operators
+# =======================================
+
+# a = 10
+# b = a
+# b //= a
+
+# print(b)
