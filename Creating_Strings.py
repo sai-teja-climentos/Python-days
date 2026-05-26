80c20e85be79e959f060ddc228efa79e32f02c28
sai-teja-climentos <saiteja.b@climentos.com>
2026-05-26 12:10:15 +0530

    day-4
    Creating Strings Notes ++++

---
+++ Creating_Strings.py
@@ -0,0 +1,153 @@
+# name = "Ram"
+# city = "Bengaluru"
+# message = "welcome to python.."
+
+
+# name = "S" + name[3:]
+
+# print(name)
+# print("===========")
+# print(city)
+# print("===========")
+# print(message)
+
+# Accessing Characters (Indexing)
+
+# Each character has a position called an index.
+
+# name = "Ram"
+# city = "Bengaluru"
+# message = "welcome to python.."
+
+# print(name[1:])
+# print("===========")
+# print(city[7:])
+# print("===========")
+# print(message[11:])
+
+
+# String Concatenation
+
+# Joining strings using +
+
+# name = "Ram"
+# city = "Bengaluru"
+# message = "welcome to python.."
+
+
+# res = name+" "+city+" "+message
+
+# print(res)
+
+# Repeating Strings
+
+# Using * operator
+
+# name = "Ram"
+
+# res = name + " "
+
+# print(res * 3)
+
+
+# upper()
+
+# Converts text to uppercase.
+
+# lower()
+
+# Converts text to lowercase.
+
+
+# name_0 = "ram"
+# name_1 = "SAM"
+
+# print(name_0[0].upper()+name_0[1:])
+
+# # print(name_0.upper())
+# # print(name_1.lower())
+
+# strip()
+
+# Removes spaces from beginning and end.
+
+# name_0 = " ram "
+
+# res = name_0.strip()
+
+# print(res)
+# print(name_0)
+
+
+# strip()
+
+# Removes spaces from beginning and end.
+
+# name_0 = "I Like Java"
+
+# res = name_0.replace("Java", "Python")
+
+# print(res)
+
+# split()
+
+# Splits string into a list.
+
+# name_0 = "apple,banana,mango"
+
+# res = name_0.split(",")
+
+# print(res[0])
+
+# find()
+
+# Finds position of a substring.
+
+# name_0 = "apple"
+
+# res =  name_0.find("P")
+
+# print(res)
+
+# startswith() and endswith()
+
+
+# name_0 = "Python"
+
+# res_1 = name_0.startswith("Py")
+# res_2 = name_0.endswith("on")
+
+# print(res_1)
+# print(res_2)
+
+# String Formatting
+# Using f-strings
+
+# name = "xyz"
+# age = 123
+
+# print(f"My name is {name} and I am {age} Years old...")
+
+
+# Checking String Properties
+
+# name = "Python1234,"
+
+# res = name.isalpha()   #only letters
+# res_1 = name.isdigit() #only numbers
+# res_2 = name.isalnum() #only numbers + letters
+
+# print(res)
+# print(res_1)
+# print(res_2)
+
+# Length of a String
+# name = "Python1234,"
+
+# print(len(name))
+
+
+
+
+
+
