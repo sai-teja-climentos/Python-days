40c6c4e5de7df26f616a2405b951593f0b06027c
sai-teja-climentos <saiteja.b@climentos.com>
2026-05-27 15:56:55 +0530

    day-5++
    day-5++

---
+++ Conditionals.py
@@ -0,0 +1,109 @@
+# # if-else Statement
+
+# # Executes one block if the condition is true, otherwise another block.
+
+# Num = 5
+
+# if Num % 2 == 0:
+    
+#     print("Even Number")
+
+# else:
+
+#     print("Odd Number")
+
+
+# # if-elif-else Statement
+
+# # Used when checking multiple conditions.
+
+# marks = 75
+
+# if marks >= 90:
+#     print("Grade A")
+# elif marks >= 70:
+#     print("Grade B")
+# elif marks >= 50:
+#     print("Grade C")
+# else:
+#     print("Fail")
+
+# # match-case Statement (Python 3.10+)
+
+# # Used for pattern matching and cleaner multiple-condition handling.
+
+# Day = 4
+
+
+# match Day:
+
+#      case 1:
+#        print("Monday")
+
+#      case 2:
+#         print("Tuesday")
+
+#      case 3:
+#         print("Wednesday")
+
+#      case _:
+#         print("Ivalid-day")
+
+
+# age = 61 
+
+# if age >=18:
+#     print("Working age")
+# elif age >=60:
+#     print("Not a Working age")
+
+
+# age = 61 
+
+# if age >= 18 and age <= 60:
+#     print("Working age..")
+
+
+# age = 60 
+
+# if age >= 18 and age <= 60:
+#     print("Working age")
+
+# elif age >= 60:
+#     print("Not a Working age")
+
+
+
+
+# for variable in sequence:
+i=1
+
+# for i in range(20):
+#     print(i)
+
+
+# # while Loop
+
+
+# num = 1 
+
+# while num <=5:
+#     print("*")
+#     num += 7
+
+# Loop Control Statements
+
+# for i in range(10):
+#     if i == 5:
+#         break
+#     print(i)
+
+
+for i in range(10):
+    if i == 5:
+        break
+    print(i)
+
+
+
+
